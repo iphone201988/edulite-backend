291b71db76cff5b0a6510dc8a6daf8a01e5178eb
Inder <iphone201988@gmail.com>
2026-01-20 17:20:43 +0530

    integrating multers3

---
+++ src/middleware/multerS3.middleware.ts
@@ -0,0 +1,33 @@
+import { S3Client } from "@aws-sdk/client-s3";
+import multer from "multer";
+import multerS3 from "multer-s3";
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config();
+
+const s3 = new S3Client({
+    region: process.env.AWS_REGION,
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID || "",
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "",
+    },
+});
+console.log("s3......",s3)
+
+const upload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: process.env.AWS_BUCKET_NAME || "",
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        metadata: function (req, file, cb) {
+            cb(null, { fieldName: file.fieldname });
+        },
+        key: function (req, file, cb) {
+            const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+            cb(null, "uploads/" + uniqueSuffix + path.extname(file.originalname));
+        },
+    }),
+});
+
+export default upload;
--- src/routes/upload.router.ts
+++ src/routes/upload.router.ts
@@ -1,18 +1,19 @@
 import { Router, Request, Response } from "express";
 import path from "path";
-import { upload } from "../middleware/multer.middleware";
+import upload from "../middleware/multerS3.middleware";
 
 const router = Router();
 
 // Enhanced upload - handles both images AND videos
-router.post("/", upload.single("file"), (req: Request, res: Response) => {
+router.post("/", upload.single("file"), (req: any, res: Response) => {
   console.log("file..", req.file);
   
   if (!req.file) {
     return res.status(400).json({ success: false, message: "No file uploaded" });
   }
 
-  const fileUrl = `/uploads/${req.file.filename}`;
+
+  const fileUrl = `/${req.file.key}`;
   console.log("fileurl", fileUrl);
 
   // Video detection for better response
